fd366ba58109a4e8eb5e25fa939c6f9e821a37aa
Pascal Robbes <pascal.robbes@gmail.com>
2020-10-30 15:13:03 +0100

    modifs

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,10 @@
 "use strict";
 
 $(document).ready(function () {
+
   $('body').autoPadding({
     source: $('.js-header'),
   });
-
-  // TOOLTIP
 
   $('.has_tooltip').tooltip({
     placement: 'right',
@@ -19,6 +18,13 @@ $(document).ready(function () {
     var position = parseInt($(this).attr("data-position"));
     $("#carousel").carousel(position);
   });
+
+  var is_mobile = $(".carousel-control-prev-icon ").css("display")=='none';
+  if (is_mobile) {
+    $("#carousel").carousel(1);
+    $("div.carousel-header a[data-position=1]").addClass("carousel-header-active");
+    $("div.carousel-header a[data-position=0]").removeClass("carousel-header-active");
+  }
 
 
 });
