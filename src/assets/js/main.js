2a9ec51a54cd9e08b0e1c37a40afb057de1baf15
Pascal Robbes <pascal.robbes@gmail.com>
2020-11-02 19:39:50 +0100

    update

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,9 +8,13 @@ $(document).ready(function () {
 
   if ($(".carousel-control-prev-icon ").css("display") == 'none') {
     mobileActions();
-  } else {
-    desktopActions();
-  }
+  } 
+
+  $('.has_tooltip').tooltip({
+    placement: 'bottom',
+    template: '<div class="tooltip" role="tooltip"><div class="tooltip-inner"></div></div>',
+    html: true
+  })
 
   $(".carousel-header>a").on("click", function () {
     var target = $(this).attr("data-target");
@@ -23,21 +27,8 @@ $(document).ready(function () {
 });
 
 function mobileActions() {
-  
   $("div.carousel-header a[data-target=carousel-mobile-container]").addClass("carousel-header-active");
   $("div.carousel-header a[data-target=carousel-desktop-container]").removeClass("carousel-header-active");
   $("div#carousel-desktop-container").addClass('is-hidden');
   $("div#carousel-mobile-container").removeClass('is-hidden');
-  $('.has_tooltip').tooltip({
-    placement: 'auto',
-    template: '<div class="tooltip" role="tooltip"><div class="tooltip-inner"></div></div>',
-    html: true
-  })}
-
-function desktopActions() {
-  $('.has_tooltip').tooltip({
-    placement: 'bottom',
-    template: '<div class="tooltip" role="tooltip"><div class="tooltip-inner"></div></div>',
-    html: true
-  })
 }
